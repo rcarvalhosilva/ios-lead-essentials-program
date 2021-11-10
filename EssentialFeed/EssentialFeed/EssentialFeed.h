8729e80a20aa0782321da99f46598eb7a1dbab1b
Rodrigo Carvalho <rcdasilva94@gmail.com>
2021-11-10 19:07:11 -0300

    Create EssentialFeed project

---
+++ EssentialFeed/EssentialFeed/EssentialFeed.h
@@ -0,0 +1,16 @@
+//
+//  EssentialFeed.h
+//  EssentialFeed
+//
+//  Created by Rodrigo Carvalho on 10/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeed.
+FOUNDATION_EXPORT double EssentialFeedVersionNumber;
+
+//! Project version string for EssentialFeed.
+FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
