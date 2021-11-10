b53c2defa03dfee6b184353c21f0ec7bbf75c052
Rodrigo Carvalho <rcdasilva94@gmail.com>
2021-11-10 19:08:48 -0300

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Rodrigo Carvalho on 10/11/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
