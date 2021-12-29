cf9413ea31959efeba54d8c84f6ceca048bbf337
Rodrigo Carvalho <rcdasilva94@gmail.com>
2021-12-29 10:18:29 -0300

    Nest `LoadFeedResult` into the FeedLoader protocol as `FeedLoader.Result` since they're closely related

--- EssentialFeed/EssentialFeedEndToEndTests/EssentialFeedEndToEndTests.swift
+++ EssentialFeed/EssentialFeedEndToEndTests/EssentialFeedEndToEndTests.swift
@@ -26,7 +26,7 @@ class EssentialFeedEndToEndTests: XCTestCase {
 
     // MARK: - Helpers
 
-    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> FeedLoader.Result? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient(session: .init(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
@@ -34,7 +34,7 @@ class EssentialFeedEndToEndTests: XCTestCase {
         trackForMemoryLeaks(loader, file: file, line: line)
         let exp = expectation(description: "Wait for load completion")
 
-        var receivedResult: LoadFeedResult?
+        var receivedResult: FeedLoader.Result?
         loader.load { result in
             receivedResult = result
             exp.fulfill()
