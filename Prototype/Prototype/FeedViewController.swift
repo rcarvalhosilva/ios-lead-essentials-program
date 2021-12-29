3b779fcaa78ba94c5b88b7cf4e35ebf6bf874992
Rodrigo Carvalho <rcdasilva94@gmail.com>
2021-12-29 16:29:26 -0300

    Render prototype feed data on the table view

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -14,11 +14,28 @@ struct FeedImageViewModel {
 }
 
 final class FeedViewController: UITableViewController {
+    private let feed = FeedImageViewModel.prototypeFeed
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        feed.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath) as! FeedImageCell
+        let model = feed[indexPath.row]
+        cell.configure(with: model)
+        return cell
+    }
+}
+
+extension FeedImageCell {
+    func configure(with viewModel: FeedImageViewModel) {
+        locationLabel.text = viewModel.location
+        locationContainer.isHidden = viewModel.location == nil
+
+        descriptionLabel.text = viewModel.description
+        descriptionLabel.isHidden = viewModel.description == nil
+
+        feedImageView.image = UIImage(named: viewModel.imageName)!
     }
 }
