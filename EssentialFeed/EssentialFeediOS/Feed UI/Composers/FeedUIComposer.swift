9ee64bb123de2c145e380dd01e0952531c12a2cf
Rodrigo Carvalho <rcdasilva94@gmail.com>
2022-01-15 18:34:40 -0300

    Remove unused `FeedViewModel` (it was replaced by the new `FeedPresenter`).

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -5,24 +5,12 @@ public final class FeedUIComposer {
     private init() {}
 
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let viewModel = FeedViewModel(feedLoader: feedLoader)
         let presenter = FeedPresenter(feedLoader: feedLoader)
         let refreshController = FeedRefreshViewController(presenter: presenter)
         let feedController = FeedViewController(refreshController: refreshController)
-        viewModel.onFeedLoaded = adaptFeedToCellControllers(forwardingTo: feedController, loader: imageLoader)
         presenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
         presenter.loadingView = WeakRefVirtualProxy(refreshController)
         return feedController
-    }
-
-    private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void  {
-        { [weak controller] feed in
-            controller?.tableModel = feed.map { model in
-                return FeedImageCellController(
-                    viewModel: FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init)
-                )
-            }
-        }
     }
 }
 
