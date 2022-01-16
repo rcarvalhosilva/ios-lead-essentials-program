03013af3ccf8b8ca54a3be127db1d5f1c5f02c1f
Rodrigo Carvalho <rcdasilva94@gmail.com>
2022-01-16 17:20:10 -0300

    Reuse tableView cells from Storyboard configuration

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -12,22 +12,24 @@ final class FeedImageCellController: FeedImageView {
         self.delegate = delegate
     }
     
-    private lazy var cell = FeedImageCell()
+    private var cell: FeedImageCell?
 
-    func view() -> UITableViewCell {
+    func view(in tableView: UITableView) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedImageCell") as! FeedImageCell
+        self.cell = cell
         delegate.didRequestImage()
         return cell
     }
 
     func display(_ viewModel: FeedImageViewModel<UIImage>) {
-        cell.locationContainer.isHidden = !viewModel.hasLocation
-        cell.locationLabel.text = viewModel.location
-        cell.descriptionLabel.text = viewModel.description
-        cell.feedImageContainer.isShimmering = viewModel.isLoading
-        cell.feedImageRetryButton.isHidden = !viewModel.shouldRetry
-        cell.feedImageView.image = viewModel.image
+        cell?.locationContainer.isHidden = !viewModel.hasLocation
+        cell?.locationLabel.text = viewModel.location
+        cell?.descriptionLabel.text = viewModel.description
+        cell?.feedImageContainer.isShimmering = viewModel.isLoading
+        cell?.feedImageRetryButton.isHidden = !viewModel.shouldRetry
+        cell?.feedImageView.image = viewModel.image
 
-        cell.onRetry = delegate.didRequestImage
+        cell?.onRetry = delegate.didRequestImage
     }
 
     func preload() {
@@ -35,6 +37,11 @@ final class FeedImageCellController: FeedImageView {
     }
 
     func cancelLoad() {
+        releaseCellForReuse()
         delegate.didCancelImageRequest()
+    }
+
+    private func releaseCellForReuse() {
+        cell = nil
     }
 }
