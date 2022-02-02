fad68357adfec5798fe4e64b64a9ab180e0a8490
Rodrigo Carvalho <rcdasilva94@gmail.com>
2022-02-02 10:03:54 -0300

    Hide error message on tap

--- EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -35,7 +35,7 @@ public final class ErrorView: UIView {
         }
     }
 
-    private func hideMessageAnimated() {
+    @IBAction private func hideMessageAnimated() {
         UIView.animate(
             withDuration: 0.25,
             animations: { self.alpha = 0 },
