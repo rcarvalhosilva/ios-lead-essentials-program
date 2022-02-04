4559141896c7b37d1d81223cfe10e136d42e94c0
Rodrigo Carvalho <rcdasilva94@gmail.com>
2022-02-04 00:27:42 -0300

    `FeedImagePresenter` displays loading image on `didStartLoadingImage`

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -1,13 +1,48 @@
 import XCTest
+import EssentialFeed
+
+protocol FeedImageView {
+    func display(_ model: FeedImageViewModel)
+}
 
 final class FeedImagePresenter {
-    init(view: Any) {
+    private let view: FeedImageView
 
+    init(view: FeedImageView) {
+        self.view = view
+    }
+
+    func didStartLoadingImage(for model: FeedImage) {
+        let viewModel = FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: true,
+            shouldRetry: false
+        )
+
+        view.display(viewModel)
     }
 }
 
-final class ViewSpy {
-    let messages = [Any]()
+struct FeedImageViewModel {
+     let description: String?
+     let location: String?
+     let image: Any?
+     let isLoading: Bool
+     let shouldRetry: Bool
+
+     var hasLocation: Bool {
+         return location != nil
+     }
+ }
+
+final class ViewSpy: FeedImageView {
+    private(set) var messages = [FeedImageViewModel]()
+
+    func display(_ model: FeedImageViewModel) {
+        messages.append(model)
+    }
 }
 
 class FeedImagePresenterTests: XCTestCase {
@@ -16,5 +51,21 @@ class FeedImagePresenterTests: XCTestCase {
         let _ = FeedImagePresenter(view: view)
 
         XCTAssertTrue(view.messages.isEmpty, "Expected no messages to view")
+    }
+
+    func test_didStartLoadingImage_displaysLoadingImage() {
+        let view = ViewSpy()
+        let image = uniqueImage()
+        let sut = FeedImagePresenter(view: view)
+
+        sut.didStartLoadingImage(for: image)
+
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertNil(message?.image)
+        XCTAssertEqual(message?.shouldRetry, false)
+        XCTAssertEqual(message?.isLoading, true)
+        XCTAssertEqual(message?.location, image.location)
+        XCTAssertEqual(message?.description, image.description)
     }
 }
