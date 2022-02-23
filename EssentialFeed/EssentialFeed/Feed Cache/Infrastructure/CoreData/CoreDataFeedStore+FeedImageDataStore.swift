0c4ff71dca1b061722e5c813a6df5499d43c9752
Rodrigo Carvalho <rcdasilva94@gmail.com>
2022-02-22 22:38:45 -0300

    Remove unnecessary return statements

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
@@ -4,7 +4,7 @@ extension CoreDataFeedStore: FeedImageDataStore {
     public func retrive(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
         perform { context in
             completion(Result {
-                return try ManagedFeedImage.first(with: url, in: context)?.data
+                try ManagedFeedImage.first(with: url, in: context)?.data
             })
         }
     }
