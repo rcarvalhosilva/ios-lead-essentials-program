4fdfef42eddaf712658367f39eec4633d4aaa299
Rodrigo Carvalho <rcdasilva94@gmail.com>
2022-03-11 19:54:19 -0300

    Record feed with failed image loading snapshot

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -21,19 +21,27 @@ class FeedSnapshotTests: XCTestCase {
 
     func test_feedWithContent() {
         let sut = makeSUT()
-
+        
         sut.display(feedWithContent())
 
         record(snapshot: sut.snapshot(), named: "FEED_WITH_CONTENT")
     }
     
     func test_feedWithErrorMessage() {
-            let sut = makeSUT()
+        let sut = makeSUT()
 
-            sut.display(.error(message: "This is a\nmulti-line\nerror message"))
+        sut.display(.error(message: "This is a\nmulti-line\nerror message"))
 
-            record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
-        }
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
+    }
+
+    func test_feedWithFailedImageLoading() {
+        let sut = makeSUT()
+
+        sut.display(feedWithFailedImageLoading())
+
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
+    }
 
     // MARK: - Helpers
     private func makeSUT() -> FeedViewController {
@@ -59,6 +67,21 @@ class FeedSnapshotTests: XCTestCase {
                 description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
                 location: "Garth Pier",
                 image: UIImage.make(withColor: .green)
+            )
+        ]
+    }
+
+    private func feedWithFailedImageLoading() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: nil,
+                location: "Cannon Street, London",
+                image: nil
+            ),
+            ImageStub(
+                description: nil,
+                location: "Brighton Seafront",
+                image: nil
             )
         ]
     }
